b477cec1e8111d7f989f4c0f3be5892ea64e66bd
Scetch <tokisakiscetch@gmail.com>
2025-04-14 20:36:55 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <ctime>
+
+const int min{ 1 };
+const int max{ 10 };
+
+int guess_randomize()
+{
+	std::string yes_or_no;
+	std::cout << "Ready to start? Type y/n: ";
+	std::cin >> yes_or_no;
+
+	if (yes_or_no == "n" || yes_or_no == "N")
+	{
+		std::cout << ":(" << std::endl;
+		return 1;
+	}
+
+	else if (yes_or_no == "y" || yes_or_no == "Y")
+	{
+		std::cout << "Lets start our game!" << std::endl;
+		do
+		{
+			int x;
+			int random_number{ rand() % (max - min + 1) + min };
+			std::cout << "Random number(from 1 to 10) was created. Try to guess: ";
+			std::cin >> x;
+			if (x != random_number)
+			{
+				std::cout << "Nice try, but u are wrong. Random number is " << random_number << std::endl;
+			}
+			else
+			{
+				std::cout << "Good job. U are right." << std::endl;
+			}
+			std::cout << "Wanna to continue? Type y/n: ";
+			std::cin >> yes_or_no;
+		}			
+		while (yes_or_no == "y" || yes_or_no == "Y");
+	}
+
+	else
+	{
+		std::cout << "Incorrect input. Code canceled." << std::endl;
+		return 1;
+	}
+
+}
+
+int main()
+{
+	srand(time(0));
+	guess_randomize();
+}
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void print(const std::vector<int>& data)
+{
+    for (const auto& n : data)
+    {
+        std::cout << n << "\t";
+    }
+    std::cout << std::endl;
+}
+
+bool is_negative(int n) { return n < 0; }
+
+int main()
+{
+    std::vector<int> numbers{ -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
+
+    // применяем алгоритм remove_if() для удаления всех элементов, которые не соответствуют условию
+    auto iter{ std::remove_if(begin(numbers), end(numbers), is_negative) };
+    print(numbers);   // 0       1       2       3       4       5       1       2       3       4       5
+
+    // удаляем все элементы, начиная с итератора first_to_erase
+    numbers.erase(iter, end(numbers));
+
+    print(numbers);   // 0       1       2       3       4       5
+}
